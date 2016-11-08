b143246c56422caa0849cd1996618cccca0adfaf
Oleksandra Puravets <o.puravets@levi9.com>
2016-11-08 16:24:25 +0200

    UPD

--- accountsList.js
+++ accountsList.js
@@ -10,14 +10,19 @@ angular.module("accountsViewer").component("accountsList", {
 
 	template: '<div class="list-container">\
     <ul class="accounts-list">\
-        <li ng-repeat="item in $ctrl.accounts" class="accounts-item">\ 
-        	<img ng-src="{{item.avatarUrl}}">\ 
+        <li ng-repeat="item in $ctrl.accounts" class="accounts-item">\
+        	<img ng-src="{{item.avatarUrl}}">\
         	<div>{{item.accountId}} </div>\
         	<div>{{item.email}} </div>\
         	<div>{{item.login}} </div>\
         	<div>{{item.name}} </div>\
         </li>\
     </ul>\
-</div>'
+</div>',
+
+    bindings: {
+		accounts: '<'
+
+	} 
 
 });
