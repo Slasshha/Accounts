a067309b80c8281aec3faab7f66ecba3df3e6a3c
Oleksandra Puravets <o.puravets@levi9.com>
2016-11-08 15:39:33 +0200

    Moved template from separate html file to inline hardcode

--- accountsList.js
+++ accountsList.js
@@ -1,7 +1,23 @@
 angular.module("accountsViewer").component("accountsList", {
+
+	/*
 	templateUrl: "accounts_list.html",
 	bindings: {
 		accounts: '<'
 
-	}
+	} */
+
+
+	template: '<div class="list-container">\
+    <ul class="accounts-list">\
+        <li ng-repeat="item in $ctrl.accounts" class="accounts-item">\ 
+        	<img ng-src="{{item.avatarUrl}}">\ 
+        	<div>{{item.accountId}} </div>\
+        	<div>{{item.email}} </div>\
+        	<div>{{item.login}} </div>\
+        	<div>{{item.name}} </div>\
+        </li>\
+    </ul>\
+</div>'
+
 });
