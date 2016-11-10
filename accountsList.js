6a9c8a51ad930ec2d6060ce007cc7902e2d9e306
Oleksandra Puravets <o.puravets@levi9.com>
2016-11-10 16:31:28 +0200

    Added hiding line when clicking on same element

--- accountsList.js
+++ accountsList.js
@@ -25,7 +25,13 @@ angular.module("accountsViewer").component("accountsList", {
 
 		$ctrl.expandDetails = function (account) {
 			console.log("click done");
-			$ctrl.activeAccountId = account.accountId;
+			if (account.accountId===$ctrl.activeAccountId) {
+				console.log("they are equal");
+				console.log($ctrl.activeAccountId);
+				$ctrl.activeAccountId = null;
+			} else {
+				$ctrl.activeAccountId = account.accountId;
+				console.log("not same")};
 
 		};
 
