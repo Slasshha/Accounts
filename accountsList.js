6b106be795d42a91509043d10bf43dc56a8ef355
Oleksandra Puravets <o.puravets@levi9.com>
2016-11-10 13:05:59 +0200

    Upd on click behavior

--- accountsList.js
+++ accountsList.js
@@ -10,13 +10,10 @@ angular.module("accountsViewer").component("accountsList", {
 
 	template: '<div class="list-container">\
     <ul class="accounts-list">\
-        <li ng-repeat="item in $ctrl.accounts" class="accounts-item">\
-        	<img ng-src="{{item.avatarUrl}}">\
-        	<div>{{item.accountId}} </div>\
-        	<div>{{item.email}} </div>\
-        	<div>{{item.login}} </div>\
-        	<div>{{item.name}} </div>\
-        </li>\
+            <li ng-repeat="item in $ctrl.accounts" class="accounts-item" style="margin: 20px" ng-click="vm.expandDetails()">\
+        	<div><img ng-src="{{item.avatarUrl}}" width="75px"> Account ID: {{item.accountId}} Email: {{item.email}} Login: {{item.login}} Name: {{item.name}}  </div>\
+        	<div class="accountDetails" ng-if="item.shown"> Age: {{item.age}}  Gender: {{item.gender}}  Phone number: {{item.phone}} </div>\
+        	</li>\
     </ul>\
 </div>',
 
